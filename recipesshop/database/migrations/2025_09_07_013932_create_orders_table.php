e8a0118caf25cf50683e6f93591c4f76a91c87a7
VanjaRikic <vanjarikic03@gmail.com>
2026-01-13 00:11:11 +0100

    izmena i dodavanje migracije (order)

--- recipesshop/database/migrations/2025_09_07_013932_create_orders_table.php
+++ recipesshop/database/migrations/2025_09_07_013932_create_orders_table.php
@@ -9,13 +9,16 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+     public function up()
     {
         Schema::create('orders', function (Blueprint $table) {
-            $table->id();
-            $table->decimal('total_amount', 12, 2)->default(0);
+            $table->bigIncrements('order_id');
+            $table->unsignedBigInteger('user_id');
             $table->string('status')->default('pending');
+            $table->decimal('total_price', 8, 2)->default(0);
             $table->timestamps();
+
+            $table->foreign('user_id')->references('user_id')->on('users')->onDelete('cascade');
         });
     }
 
---
+++ recipesshop/database/migrations/2026_01_12_215622_create_order_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->bigIncrements('order_item_id');
+            $table->unsignedBigInteger('order_id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('ingredient_id');
+            $table->integer('amount');
+            $table->decimal('total_price', 8, 2);
+            $table->timestamps();
+
+            $table->foreign('order_id')->references('order_id')->on('orders')->onDelete('cascade');
+            $table->foreign('user_id')->references('user_id')->on('users')->onDelete('cascade');
+            $table->foreign('ingredient_id')->references('ingredient_id')->on('ingredients')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_items');
+    }
+};
