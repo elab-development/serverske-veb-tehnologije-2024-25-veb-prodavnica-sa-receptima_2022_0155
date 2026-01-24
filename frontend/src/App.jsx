82e202e9f96d5df03874bd90ab3a7bdbec040ab3
JovanaRadisavljevic <radisavljevic435@gmail.com>
2026-01-24 19:30:21 +0100

    kreirana login stranica

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
 
@@ -13,6 +13,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   )
